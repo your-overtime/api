db9c1b6cdb010b89ecb7126a5ffb63ed6c763035
jasper Eumann <git@jasper-eumann.de>
2021-06-20 23:07:54 +0200

    🐛 fix overlap and activities (close #1)

--- internal/data/hollyday.go
+++ internal/data/hollyday.go
@@ -23,11 +23,7 @@ func (d *Db) GetHollyday(id uint) (*pkg.Hollyday, error) {
 
 func (d *Db) GetHollydaysBetweenStartAndEnd(start time.Time, end time.Time, employeeID uint) ([]pkg.Hollyday, error) {
 	hollydays := []pkg.Hollyday{}
-	tx := d.Conn.Where("user_id = ?", employeeID).Where("start between ? and ?", start, end).
-		Or(
-			d.Conn.Where("end between ? and ?", start, end).
-				Where("start not between ? and ?", start, end),
-		).Find(&hollydays)
+	tx := d.Conn.Where("user_id = ?", employeeID).Where("? < end AND ? > start", start, end).Find(&hollydays)
 	if tx.Error != nil && tx.Error != sql.ErrNoRows {
 		return nil, tx.Error
 	}
