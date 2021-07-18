b7489b5baada9f3c95a744963d859ff31051611c
jasper Eumann <git@jasper-eumann.de>
2021-07-18 20:16:57 +0200

    ✅ add simple overview test

--- internal/service/overtime_test.go
+++ internal/service/overtime_test.go
@@ -1,7 +1,6 @@
 package service_test
 
 import (
-	"encoding/json"
 	"testing"
 
 	"github.com/your-overtime/api/internal/service"
@@ -34,7 +33,7 @@ func setUp(t *testing.T) (pkg.OvertimeService, *pkg.Employee) {
 
 func TestOverview(t *testing.T) {
 	s, ePtr := setUp(t)
-	o, err := s.CalcOverview(*ePtr, tests.ParseDay("2021-01-08"))
+	o, err := s.CalcOverview(*ePtr, tests.ParseDayTime("2021-01-08 23:59"))
 	if err != nil {
 		t.Fatal("expect no error but got ", err)
 	}
@@ -68,6 +67,58 @@ func TestOverview(t *testing.T) {
 		t.Error("expect nil but got ", o.ActiveActivity)
 	}
 
-	x, _ := json.MarshalIndent(&o, " ", "")
-	t.Log(string(x))
+	start := tests.ParseDayTime("2021-01-09 07:04")
+	end := tests.ParseDayTime("2021-01-09 08:08")
+	_, err = s.AddActivity(pkg.Activity{
+		Start:       &start,
+		End:         &end,
+		Description: "Tests",
+		UserID:      ePtr.ID,
+	}, *ePtr)
+
+	if err != nil {
+		t.Fatal("expect no error but got ", err)
+	}
+
+	o, err = s.CalcOverview(*ePtr, tests.ParseDayTime("2021-01-09 23:59"))
+	if err != nil {
+		t.Fatal("expect no error but got ", err)
+	}
+
+	if o.OvertimeThisDayInMinutes != -320 {
+		t.Error("expect -320 but got ", o.OvertimeThisDayInMinutes)
+	}
+
+	if o.ActiveTimeThisDayInMinutes != 64 {
+		t.Error("expect 64 but got ", o.ActiveTimeThisDayInMinutes)
+	}
+
+	// 384 * 4 - 64 = 1472
+	if o.OvertimeThisWeekInMinutes != -1472 {
+		t.Error("expect -1472 but got ", o.OvertimeThisWeekInMinutes)
+	}
+
+	if o.ActiveTimeThisWeekInMinutes != 64 {
+		t.Error("expect 64 but got ", o.ActiveTimeThisWeekInMinutes)
+	}
+
+	if o.OvertimeThisMonthInMinutes != -2240 {
+		t.Error("expect -2250 but got ", o.OvertimeThisMonthInMinutes)
+	}
+
+	if o.ActiveTimeThisMonthInMinutes != 64 {
+		t.Error("expect 64 but got ", o.ActiveTimeThisMonthInMinutes)
+	}
+
+	if o.OvertimeThisYearInMinutes != -2240 {
+		t.Error("expect -2240 but got ", o.OvertimeThisYearInMinutes)
+	}
+
+	if o.ActiveTimeThisYearInMinutes != 64 {
+		t.Error("expect 64 but got ", o.ActiveTimeThisYearInMinutes)
+	}
+
+	if o.ActiveActivity != nil {
+		t.Error("expect nil but got ", o.ActiveActivity)
+	}
 }
